06fec711ef0ad357cf7dd1e77584639e76d0bde2
cgrossi <cgrossi@gmail.com>
2020-01-07 14:51:35 -0700

    Completed part 3.6

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-const persons = [
+app.use(bodyParser.json())
+
+let persons = [
   {
     name: 'CArmine',
     number: 4039017458,
@@ -25,14 +28,61 @@ const persons = [
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.status(200).send(persons)
+  res.send(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = +req.params.id
+  const person = persons.find(person => person.id === id)
+  if(person) {
+    res.send(person)
+  } else {
+    res.status(400).send(`Person with an id of ${id} could not be found`)
+  }
 })
 
 app.get('/info', (req, res) => {
   let length = persons.length
   let date = new Date()
-  console.log(date)
   res.send(`<p>Phonebook has info for ${length} people</p><br>${date}`)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const id = Math.floor(Math.random() * 20000)
+
+  if(!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'Error, missing name or number'
+    })
+  }
+
+  const isName = persons.find(person => person.name === body.name)
+
+  if(isName) {
+    return res.status(400).json({
+      error: 'Error, name is already in the phonebook'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: id
+  }
+  persons = persons.concat(person)
+  res.json(persons)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = +req.params.id
+  const person = persons.find(person => person.id === id)
+  if(person) {
+    persons = persons.filter(person => person.id !== id)
+    res.send(`${person.name} was successfully deleted from the phonebook`)
+  } else {
+    res.status(400).send(`A person with that id could not be found`)
+  }
 })
 
 const PORT = 3001
