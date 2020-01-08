f23773bb518f95bc126e4a21e4a3585d31bb623a
cgrossi <cgrossi@gmail.com>
2020-01-08 13:41:57 -0700

    Completed part 2.8

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+morgan.token('person', (req, res) => { 
+  return JSON.stringify(req.body) 
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
   {
